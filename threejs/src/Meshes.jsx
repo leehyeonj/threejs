8cf22246fca300b0fe0921059f9da5f3c9b455a5
leehyeonj <billaa@naver.com>
2021-12-29 11:18:42 +0900

    feat : react-three/fiber basic

--- threejs/src/Meshes.jsx
+++ threejs/src/Meshes.jsx
@@ -1,37 +1,22 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+//import {OrbitControls} from '@react-three/drei';
+function CoinMesh() {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.1));
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    <mesh ref={mesh} scale={3}>
+      <cylinderBufferGeometry color="red" args={[1, 1, 0.3, 50]} />
+      <meshLambertMaterial attach="material" />
     </mesh>
-  )
+  );
 }
-
-export default function App() {
+export default function Meshes() {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
-  )
+    <>
+      <Canvas>
+        <CoinMesh />
+      </Canvas>
+    </>
+  );
 }
